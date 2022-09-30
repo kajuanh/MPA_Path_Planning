9f09c0ac1d8b8c69b404d33cbdc1df9980b05a93
kajuanh <kajuanh2000@gmail.com>
2022-09-30 17:24:59 +0700

    test code

--- modify_code.py
+++ modify_code.py
@@ -1,4 +1,5 @@
 import math
+from typing import List, Union
 import numpy as np
 import random as rd
 import time
@@ -36,8 +37,8 @@ class Node:
       |x = xA+a*t (a = xB - xA)
       |y = yA+b*t (b = yB - yA)
 
-    if t = 1 : x = xB, y = yB => AB ~ (0<delta_t<1)
-    '''
+    if t = 1 : x = xB, y = yB => AB ~ (0<delta_t<1)  
+    '''     
     self.a = x-self.x
     self.b = y-self.y
     # self.delta_t = 1/(self.a*self.b) if self.a !=0 and self.b != 0 else 0.1
@@ -74,29 +75,28 @@ class Node:
   def collision_coordinates(self, environment: np, end) -> list[tuple]:
     collision = []
     t = 0
-    while t < 1-0.05:
+    while t<1-0.05:
       t += 0.05
-      if self.a != 0 and self.b != 0:
-        location_x = math.floor(self.x + self.a*t)
-        location_y = math.floor(self.y + self.b*t)
+      if self.a!=0 and self.b!=0:
+        location_x =  math.floor(self.x + self.a*t)
+        location_y =  math.floor(self.y + self.b*t)
 
         if environment[location_x][location_y] == 1:
-          collision.append((location_x, location_y))
+          collision.append((location_x,location_y))
         for x, y in self.three_corners:
             x = math.floor(x + self.a*t)
             y = math.floor(y + self.b*t)
             if environment[x][y] == 1:
-              collision.append((x, y))
+              collision.append((x,y))
       else:
-        location_x = math.floor(self.x + self.a*t)
-        location_y = math.floor(self.y + self.b*t)
+        location_x =  math.floor(self.x + self.a*t)
+        location_y =  math.floor(self.y + self.b*t)
         if environment[location_x][location_y] == 1:
-          collision.append((location_x, location_y))
+          collision.append((location_x,location_y))
 
     collision = list(set(collision))
     collision.sort()
     return collision
-
 
 class MPA:
   environment = None
@@ -112,6 +112,7 @@ class MPA:
     with open(filepath, "r") as file:
       #area = map_size*map_size (map_size located on line 1 of the file)
       map_size = int(file.readline())
+
       #number of goal (num_goal located on line 2 of the file)
       num_goal = int(file.readline())
 
@@ -139,7 +140,7 @@ class MPA:
             list_obstacle.append((i, j))
           else:
             list_empty.append((i, j))
-
+                   
       #change node goal to empty
       for goal in list_goal:
         np_map[int(goal[0])][int(goal[1])] = 0
@@ -150,23 +151,22 @@ class MPA:
       self.goals = list_goal
       self.environment = np_map
       self.map_size = map_size
-
+    
   # def collision(self, x1, y1, x2, y2):
 
-  def check_collision(self, st_p: TypeVar('', list, tuple), en_p: tuple or list):
+  def check_collision(self, st_point: Union[tuple[int, int], list[int, int]], en_point: Union[tuple[int, int], list[int, int]]) -> bool:
+    x1 = st_point[0]
+    y1 = st_point[1]
+    x2 = en_point[0]
+    y2 = en_point[1]
     ''''''
-    x1 = st_p[0]
-    y1 = st_p[1]
-    x2 = en_p[0]
-    y2 = en_p[1]
-
     if x1 >= self.map_size or y1 >= self.map_size or x2 >= self.map_size or y2 >= self.map_size:
       return True
     if x1 < 0 or y1 < 0 or x2 < 0 or y2 < 0:
       return True
     if self.environment[x1][y1] == 1 or self.environment[x2][y2] == 1:
       return True
-
+    
     #convert to Node
     node = Node(x1, y1)
     node.linear_equations_to(x2, y2)
@@ -180,7 +180,7 @@ class MPA:
     node = Node(x1, y1)
     node.linear_equations_to(x2, y2)
     return node.collision_coordinates(self.environment, (x2, y2))
-
+    
   def check(self, f_X1, f_X2):
     f_x = f_X1[0]
     f_y = f_X1[1]
@@ -251,9 +251,9 @@ class MPA:
 
     print(n_child, min_s, prey, best_prey, old_s, max_d, sep='(:-:)')
     # for index in range(10):
-    i = 3
-    limit_rd = i**2 - 1
-    for index in range(10):
+    i = 5
+    limit_rd = (i+1)**2-1
+    for index in range(n_child):
       origin_sol = [st]
       temp_map = self.environment.copy()
       temp_map[st[0]][st[1]] = 3
@@ -264,19 +264,19 @@ class MPA:
         # print(origin_sol[-1])
         limit_loop += 1
         while True:
-            limit += 1
-            x = origin_sol[-1][0] + rd.randint(-i, i)
-            y = origin_sol[-1][1] + rd.randint(-i, i)
-            if limit > limit_rd:
-              temp_map[origin_sol[-1][0]][origin_sol[-1][1]] = 0
-              origin_sol.pop()
-              limit = 0
-              break
-            if self.map_size <= x or x < 0 or self.map_size <= y or y < 0:
-              continue
-            if temp_map[x][y] == 3 or temp_map[x][y] == 1:
+              limit += 1
+              x = origin_sol[-1][0] + rd.randint(-i, i)
+              y = origin_sol[-1][1] + rd.randint(-i, i)
+              if limit > limit_rd:
+                temp_map[origin_sol[-1][0]][origin_sol[-1][1]] = 0
+                origin_sol.pop()
+                limit = 0
+                break
+              if self.map_size <= x or x < 0 or self.map_size <= y or y < 0:
                 continue
-            if not self.check_collision([x, y], origin_sol[-1]):
+              if temp_map[x][y] == 3 or temp_map[x][y] == 1:
+                  continue
+              if not self.check_collision([x, y], origin_sol[-1]):
                 origin_sol.append([x, y])
                 temp_map[x][y] = 3
                 limit = 0
@@ -318,8 +318,10 @@ class MPA:
           min_s = dis_prey
           best_prey = list(iPrey)
     for i in prey:
-      i.extend([dst for _ in range(max_d-len(i)+1)])
+      i.extend([dst for _ in range(max_d-len(i))])
     a_prey = np.array(prey)
+    for i in a_prey:
+      print(i.T)
         # if a_prey[i][j] !=prey[i][j]:
         #   print('another')
     # for i in range(10):
